64b21d85cc0431cc059ada3037b7ec68f469c51a
pinkpegasus <rukiyadykes@gmail.com>
2022-02-28 07:41:25 -0600

    imported components into app-routing

--- charity/src/app/app-routing.module.ts
+++ charity/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { CharitiesComponent } from './charities/charities.component';
+import { HomeComponent } from './home/home.component';
+import { HouseholdsComponent } from './households/households.component';
+import {ContactUsComponent} from './contact-us/contact-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", pathMatch: "full", redirectTo: "/home"},//if spelling error, makes user go to home page
+  {path: "home", component: HomeComponent},
+  {path: "about", component: AboutComponent},
+  {path: "charities", component: CharitiesComponent},
+  {path: "households",  component: HouseholdsComponent},
+  {path: "contactUs", component: ContactUsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
