4e72beae6fa3de25abdbffcf1ddf4763af754df0
pinkpegasus <rukiyadykes@gmail.com>
2022-03-01 21:04:32 -0600

    created method for api

--- charity/src/app/charity.service.ts
+++ charity/src/app/charity.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+// import { Observable } from 'rxjs';
+// import { Charities } from './charities';
+// TODO: Replace
 
 @Injectable({
   providedIn: 'root'
 })
 export class CharityService {
 
-  private baseUrl = "https://springboot-postgresql-charity.herokuapp.com/api/charities";
+
+
+  // private baseUrl = "https://springboot-postgresql-charity.herokuapp.com/api/charities";
   constructor(private http: HttpClient) { }
+
+  apiObservable(){
+    return this.http
+    .get(`https://springboot-postgresql-charity.herokuapp.com/api/charities`)
+  }
+
+  // getCharityList(): Observable<Charities[]>{
+  //   return this.http.get<Charities[]>(`${this.baseUrl}`);
+  // }
 }
